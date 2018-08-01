9968ae2bfdd2f63337d760dcc265ee9eca68f0de
ElderJames <shunjiey@hotmail.com>
2018-08-01 15:44:33 +0800

    添加项目

---
+++ src/Wizard.Cinema.Collector/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cinema.Collector
+{
+    internal class Program
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        private static async Task Main(string[] args)
+        {
+            var json = await HttpClient.GetStringAsync("http://m.maoyan.com/ajax/cinemaList?limit=1&districtId=-1&lineId=-1&hallType=-1&brandId=-1&serviceId=-1&areaId=-1&stationId=-1&item=");
+            Console.WriteLine(json);
+
+            Console.ReadKey();
+        }
+    }
+}
