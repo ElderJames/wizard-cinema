a1e957826c5508da557cc500a21499b55a7d9f73
ElderJames <shunjiey@hotmail.com>
2018-09-06 19:02:35 +0800

    feat 活动聚合

---
+++ src/Wizard.Cinema.Domain/Activity/Activity.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Wizard.Cinema.Domain.Activity.EnumTypes;
+
+namespace Wizard.Cinema.Domain.Activity
+{
+    /// <summary>
+    /// 活动
+    /// </summary>
+    public class Activity
+    {
+        /// <summary>
+        /// 活动Id
+        /// </summary>
+        public long ActivityId { get; set; }
+
+        /// <summary>
+        /// 活动名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 活动详情
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 活动地址
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 活动状态
+        /// </summary>
+        public ActivityStatus Status { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime BeginTime { get; set; }
+
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public DateTime FinishTime { get; set; }
+
+        /// <summary>
+        /// 报名开始时间
+        /// </summary>
+        public DateTime RegistrationBeginTime { get; set; }
+
+        /// <summary>
+        /// 报名截止时间
+        /// </summary>
+        public DateTime RegistrationFinishTime { get; set; }
+
+        /// <summary>
+        /// 报名费用
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 创建者id
+        /// </summary>
+        public long CreatorId { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ src/Wizard.Cinema.Domain/Activity/EnumTypes/ActivityStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wizard.Cinema.Domain.Activity.EnumTypes
+{
+    public enum ActivityStatus
+    {
+        未启动 = 0,
+        报名中 = 5,
+        报名结束 = 10,
+        进行中 = 15,
+        已结束 = 20
+    }
+}
