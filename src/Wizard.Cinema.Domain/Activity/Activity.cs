6ffe56dd815c4b4e911bed9cfc6b524a0373ef84
ElderJames <shunjiey@hotmail.com>
2018-09-07 18:33:06 +0800

    feat 活动创建方法

--- src/Wizard.Cinema.Domain/Activity/Activity.cs
+++ src/Wizard.Cinema.Domain/Activity/Activity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Wizard.Cinema.Domain.Activity.EnumTypes;
+using Wizard.Infrastructures.Exceptions;
 
 namespace Wizard.Cinema.Domain.Activity
 {
@@ -67,5 +68,43 @@ namespace Wizard.Cinema.Domain.Activity
         /// 创建时间
         /// </summary>
         public DateTime CreateTime { get; set; }
+
+        public Activity(long activityId, string name, string description, string address,
+            DateTime beginTime, DateTime finishTime, DateTime registrationBeginTime, DateTime registrationFinishTime,
+            decimal price, long creatorId, DateTime createTime)
+        {
+            this.ActivityId = activityId;
+            this.Name = name;
+            this.Description = description;
+            this.Address = address;
+            this.BeginTime = beginTime;
+            this.FinishTime = finishTime;
+            this.RegistrationBeginTime = registrationBeginTime;
+            this.RegistrationFinishTime = registrationFinishTime;
+            this.Price = price;
+            this.CreatorId = creatorId;
+            this.CreateTime = createTime;
+            this.Status = ActivityStatus.未启动;
+        }
+
+        public void Change(string name, string description, string address,
+            DateTime beginTime, DateTime finishTime, DateTime registrationBeginTime, DateTime registrationFinishTime,
+            decimal price, long creatorId, DateTime createTime)
+        {
+            if (this.Status != ActivityStatus.未启动)
+                throw new DomainException("进行中不能修改");
+
+            this.Name = name;
+            this.Description = description;
+            this.Address = address;
+            this.BeginTime = beginTime;
+            this.FinishTime = finishTime;
+            this.RegistrationBeginTime = registrationBeginTime;
+            this.RegistrationFinishTime = registrationFinishTime;
+            this.Price = price;
+            this.CreatorId = creatorId;
+            this.CreateTime = createTime;
+            this.Status = ActivityStatus.未启动;
+        }
     }
 }
