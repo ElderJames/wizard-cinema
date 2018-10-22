aa86307efa4442c55aa603f73fb9cdbdc39fa793
ElderJames <shunjiey@hotmail.com>
2018-10-22 21:41:30 +0800

    feat(web): 活动列表增加简介

--- src/Wizard.Cinema.Web/ClientApp/src/components/Index.vue
+++ src/Wizard.Cinema.Web/ClientApp/src/components/Index.vue
@@ -12,7 +12,7 @@
         mu-grid-tile(v-for="activity in activities" :cols="2" :rows="1" title-position="bottom" action-position="left" @click="onTileClick(activity)" )
           img(:src="activity.picUrl" style="height:100%;width:100%")
           span(slot="title") {{activity.name}} 
-          //- span(slot="subTitle") 首映场
+          span(slot="subTitle") {{activity.summary}} 
           mu-button(slot="action" icon)
             mu-icon(value="star_border")
 </template>
@@ -33,17 +33,20 @@ export default {
       {
         picUrl: "https://muse-ui.org/img/sun.a646a52d.jpg",
         activityId: 1,
-        name: "神期动物在哪里2"
+        name: "神期动物在哪里2",
+        summary: "首映"
       },
       {
         picUrl: "https://muse-ui.org/img/sun.a646a52d.jpg",
         activityId: 1,
-        name: "神期动物在哪里2"
+        name: "神期动物在哪里2",
+        summary: "周末"
       },
       {
         picUrl: "https://muse-ui.org/img/sun.a646a52d.jpg",
         activityId: 1,
-        name: "神期动物在哪里2"
+        name: "神期动物在哪里2",
+        summary: "三强争霸赛"
       }
     ];
   },
