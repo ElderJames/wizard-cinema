40550e81d6faebf954cdba58daf4a370d82e2fd9
ElderJames <shunjiey@hotmail.com>
2018-11-07 15:45:46 +0800

    增加任务查询接口

--- src/Wizard.Cinema.Web/Controllers/SessionController.cs
+++ src/Wizard.Cinema.Web/Controllers/SessionController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Infrastructures;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Wizard.Cinema.Application.DTOs.EnumTypes;
 using Wizard.Cinema.Application.DTOs.Response;
 using Wizard.Cinema.Application.Services;
 using Wizard.Cinema.Web.Extensions;
@@ -16,10 +17,12 @@ namespace Wizard.Cinema.Web.Controllers
     public class SessionController : ControllerBase
     {
         private readonly ISeatService _seatService;
+        private readonly ISelectSeatTaskService _selectSeatTaskService;
 
-        public SessionController(ISeatService seatService)
+        public SessionController(ISeatService seatService, ISelectSeatTaskService selectSeatTaskService)
         {
             this._seatService = seatService;
+            this._selectSeatTaskService = selectSeatTaskService;
         }
 
         [HttpPost("select-seats")]
@@ -51,6 +54,49 @@ namespace Wizard.Cinema.Web.Controllers
                 x.WizardId,
                 x.Selected,
             })));
+        }
+
+        [HttpGet("tasks")]
+        public IActionResult GetTasks(long sessionId)
+        {
+            if (sessionId <= 0)
+                return Ok(new ApiResult<object>(ResultStatus.FAIL, "请选择正确的场次"));
+
+            ApiResult<IEnumerable<SelectSeatTaskResp>> result = _selectSeatTaskService.Search(sessionId);
+            if (result.Status != ResultStatus.SUCCESS)
+                return Ok(new ApiResult<bool>(ResultStatus.FAIL, result.Message));
+
+            //当前在选的巫师
+            SelectSeatTaskResp currentWizard = result.Result.FirstOrDefault(x => x.Status == SelectTaskStatus.进行中);
+            SelectSeatTaskResp nextWizard = result.Result.Where(x => x.SerialNo >= currentWizard.SerialNo).OrderBy(x => x.SerialNo).FirstOrDefault();
+            SelectSeatTaskResp myTask = result.Result.FirstOrDefault(x => x.WizardId == HttpContext?.User?.ExtractUserId());
+            int people = result.Result.Count(x => x.SerialNo >= currentWizard.SerialNo && x.SerialNo < myTask.SerialNo);
+            int waitTime = people * 5;
+
+            return Ok(new ApiResult<object>(ResultStatus.SUCCESS, new
+            {
+                current = currentWizard == null ? null : new
+                {
+                    currentWizard.BeginTime,
+                    currentWizard.WechatName,
+                    currentWizard.Total
+                },
+                next = nextWizard == null ? null : new
+                {
+                    nextWizard.BeginTime,
+                    nextWizard.WechatName,
+                    nextWizard.Total
+                },
+                my = new
+                {
+                    myTask.BeginTime,
+                    myTask.WechatName,
+                    myTask.Total,
+                    waitTime = waitTime,
+                    people = people
+                },
+                selectedList = result.Result.Where(x => x.Status == SelectTaskStatus.已完成).SelectMany(x => x.SeatNos)
+            }));
         }
     }
 }
